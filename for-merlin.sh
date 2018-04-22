b98fdd90041628938bb2cd4a5694f859b6ebdbba
神代綺凜 <hqrqq122680216@126.com>
2018-04-22 14:54:59 +0800

    Create for-merlin.sh

---
+++ for-merlin.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+#梅林固件专用
+
+#API Key
+api_key=""
+ 
+#Secret Key
+secret_key=""
+ 
+#Domain name
+domain=""
+
+url="https://www.cloudxns.net/api2/ddns"
+time=$(date -R)
+data="{\"domain\":\"${domain}\",\"ip\":\"${1}\",\"line_id\":\"1\"}"
+mac_raw="$api_key$url$data$time$secret_key"
+mac=$(echo -n $mac_raw | md5sum | awk '{print $1}')
+header1="API-KEY:"$api_key
+header2="API-REQUEST-DATE:"$time
+header3="API-HMAC:"$mac
+header4="API-FORMAT:json"
+
+result=$(curl -k -X POST -H $header1 -H "$header2" -H $header3 -H $header4 -d "$data" $url)
+if  [[ $(echo $result | grep "success") != "" ]] ;then
+	/sbin/ddns_custom_updated 1
+else
+	/sbin/ddns_custom_updated 0
+fi
